0e82d8f0345e4b40e5cc4879fb47afa0837daecd
Ruaxin <qy.xin@foxmail.com>
2020-08-24 09:41:28 +0800

    init

---
+++ precache-manifest.11a549f5c4f509c547b29746dd6136db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb6ee70170702c4d584a22822fc8afd",
+    "url": "/bookkeeping-website/index.html"
+  },
+  {
+    "revision": "7ec104c657e3e5542fb5",
+    "url": "/bookkeeping-website/static/css/main.f22ceb10.chunk.css"
+  },
+  {
+    "revision": "3aca9bf203eb9b80c4ca",
+    "url": "/bookkeeping-website/static/js/2.32fb23be.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/bookkeeping-website/static/js/2.32fb23be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec104c657e3e5542fb5",
+    "url": "/bookkeeping-website/static/js/main.2658b9c7.chunk.js"
+  },
+  {
+    "revision": "e040ca8c9d2aa7ab4426",
+    "url": "/bookkeeping-website/static/js/runtime-main.c99b4abc.js"
+  }
+]);
